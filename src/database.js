b5d8e0528f467e3e5006b7ef0b901af31d941260
Luis Felipe <luisfelipe7014feups@gmail.com>
2024-03-25 15:19:03 -0300

    add codes in select

--- src/database.js
+++ src/database.js
@@ -4,10 +4,12 @@ export class database{
     database = {}
 
     select(table){
+        const data = this.database[table] ?? []
 
+        return data
     }
 
     insert(table, data){
-        
+
     }
 }
